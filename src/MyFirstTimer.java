3927f70493ef439596cc79ccab6bd413d702407e
Jonathan Villanueva <j_villanueva11@u.pacific.edu>
2024-10-02 21:07:25 -0700

    The three things a timer needs:
    is a timer object that notifies a  object or class after a certain
    amount of specified time passes.
    The second thing a timer need is an ActionListener that does a certain
    action every time the specified time passes in the time object.
    Lastly a start method to initiate the timer to start.

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,11 +1,17 @@
-import acm.graphics.*;
-import acm.program.*;
+import java.awt.event.ActionEvent;
+
+import javax.swing.Timer;
+
+import acm.graphics.GLabel;
+import acm.program.GraphicsProgram;
 
 public class MyFirstTimer extends GraphicsProgram {
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
 	private GLabel myLabel;
+	private int numTimes = 0;
+	private Timer someTimerVar = new Timer(1000, this);
 
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
@@ -15,6 +21,18 @@ public class MyFirstTimer extends GraphicsProgram {
 	public void run() {
 		myLabel = new GLabel("# of times called?", 0, 100);
 		add(myLabel);
+		someTimerVar.setInitialDelay(3000);
+		someTimerVar.start();
+	}
+	
+	
+	public void actionPerformed(ActionEvent e) {
+		myLabel.setLabel("times called? " + numTimes);
+		numTimes += 1;
+		myLabel.move(5,0);
+		if(numTimes == 11) {
+			someTimerVar.stop();
+		}
 	}
 	
 	public static void main(String[] args) {
